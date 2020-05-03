e04166bfe4222bd65c2d620fe27ca95b4500cb1c
LEANDRO MARQUES DA SILVA <leandrostar.01@gmail.com>
2020-05-02 22:32:31 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+// create a variable, which could be anything!
+$someVar = $nomes = array('name1' => 'leandro', 'name2' => 'silvana');
+
+dump($someVar);
+
+
+
+
+?>
